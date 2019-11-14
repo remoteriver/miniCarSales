5b8d6e10f03d8fa2509894952a9ce546522b2761
Sihe Gao <remoteriver@gmail.com>
2019-11-14 16:35:29 +1100

    WIP

---
+++ miniCarSales/Models/Car.cs
@@ -0,0 +1,27 @@
+﻿using miniCarSales.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace miniCarSales.Models
+{
+    public class Car : IVehicle
+    {
+        public long Id { get; set; }
+
+        public string VehicleType { set; get; }
+
+        public string Make { set; get; }
+
+        public string Model { set; get; }
+
+        public string Engine { set; get; }
+
+        public int Doors { set; get; }
+
+        public int Wheels { set; get; }
+
+        public string BodyType { set; get; }
+    }
+}
---
+++ miniCarSales/Models/Interface/IVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace miniCarSales.Models.Interface
+{
+    interface IVehicle
+    {
+        long Id { set; get; }
+
+        string VehicleType { set; get; }
+
+        string Make { set; get; }
+
+        string Model { set; get; }
+    }
+}

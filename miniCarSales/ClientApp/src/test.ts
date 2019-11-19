afe2bd5847ff2b754d89620c6fae2c837977f1b3
remoteriver <remoteriver@gmail.com>
2019-11-19 22:21:58 +1100

    unit test environment

--- miniCarSales/ClientApp/src/test.ts
+++ miniCarSales/ClientApp/src/test.ts
@@ -7,11 +7,45 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+//angular material 
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatSelectModule } from '@angular/material/select';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 declare const require: any;
+
+const materialModules = [
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatTreeModule,
+    MatSelectModule
+];
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
+    [BrowserDynamicTestingModule, ...materialModules, FormsModule, ReactiveFormsModule, BrowserAnimationsModule],
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
